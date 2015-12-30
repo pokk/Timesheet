fa28f88786062f32528d9a353008da55d7c7408f
Jieyi <pokkbaby@gmail.com>
2015-12-30 15:57:51 +0900

    1. Add the most of website. 2. Finished the template site.

--- sheet/urls.py
+++ sheet/urls.py
@@ -4,6 +4,22 @@ from sheet import views
 
 urlpatterns = [
 	# the 'name' value as called by the {% url %} template tag
+	# common
 	url(r'^login/$', views.login, name='login'),
-	url(r'^index/$', views.index, name='index'),
+	url(r'^index/$', views.dashboard, name='index'),
+	# user
+	# expense
+	url(r'^expense/$', views.expense, name='expense'),
+	# library
+	url(r'^library/book_list$', views.library_book_list, name='book_list'),
+	url(r'^library/rent$', views.library_rent, name='book_rent'),
+	url(r'^library/search$', views.library_search, name='book_search'),
+	# report
+	url(r'^report/daily$', views.report_daily, name='report_daily'),
+	url(r'^report/weekly$', views.report_weekly, name='report_weekly'),
+	url(r'^report/monthly$', views.report_monthly, name='report_monthly'),
+	# time sheet
+	url(r'^time-sheet/time$', views.ts_time_sheet, name='ts_time'),
+	url(r'^time-sheet/pay-holiday$', views.ts_payment_holiday, name='ts_pay'),
+	url(r'^time-sheet/working-holiday$', views.ts_working_on_holiday, name='ts_work'),
 ]
--- sheet/views.py
+++ sheet/views.py
@@ -7,7 +7,6 @@ from sheet.models import User
 
 # Create your views here.
 
-
 def login(request):
 	title = "Time Sheet Login"
 
@@ -29,5 +28,45 @@ def login(request):
 		return render_to_response('login/login.html', {'error': False})
 
 
-def index(request):
-	return render_to_response('content/__base.html', locals())
+def dashboard(request):
+	return render_to_response('content/dashboard.html', locals())
+
+
+def expense(request):
+	return render_to_response('content/expense/expense.html', locals())
+
+
+def library_book_list(request):
+	return render_to_response('content/library/book_list.html', locals())
+
+
+def library_rent(request):
+	return render_to_response('content/library/rent.html', locals())
+
+
+def library_search(request):
+	return render_to_response('content/library/search.html', locals())
+
+
+def report_daily(request):
+	return render_to_response('content/report/daily.html', locals())
+
+
+def report_monthly(request):
+	return render_to_response('content/report/monthly.html', locals())
+
+
+def report_weekly(request):
+	return render_to_response('content/report/weekly.html', locals())
+
+
+def ts_payment_holiday(request):
+	return render_to_response('content/timesheet/payholiday.html', locals())
+
+
+def ts_time_sheet(request):
+	return render_to_response('content/timesheet/timesheet.html', locals())
+
+
+def ts_working_on_holiday(request):
+	return render_to_response('content/timesheet/workingholiday.html', locals())
