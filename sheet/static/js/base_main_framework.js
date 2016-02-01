7a0dd4d581ba6d04c4cefee9ea647c092bcefcc7
Jieyi <pokkbaby@gmail.com>
2016-02-01 13:38:46 +0900

    1. Fix the navi dropdown margin.

---
+++ sheet/static/js/base_main_framework.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Jieyi on 2/1/16.
+ */
+
+$(document).ready(function ()
+{
+	// !!Drop down button.
+	$('.dropdown-button').click(function ()
+	{
+		$('#profile-dropdown')[0].style.top = '57px';
+	});
+});
